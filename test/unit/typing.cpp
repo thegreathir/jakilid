64dda416c398de7cdf27695388338b200c71dc14
thegreathir <aaph1377@gmail.com>
2020-11-10 03:59:11 +0330

    Add type test generator

--- test/unit/typing.cpp
+++ test/unit/typing.cpp
@@ -1,23 +1,78 @@
-#include <gtest/gtest.h>
+#include "utils/random.hpp"
+#include "utils/mp.hpp"
+
+#include <traits.hpp>
 #include <jakilid.hpp>
 
-#include <iostream>
+#include <boost/mpl/transform.hpp>
+#include <gtest/gtest.h>
 
-TEST(sample, sample1) {
-    unsigned short x = 17;
-    long long y = static_cast<long long>(x);
-    short z =static_cast<short>(y);
-    std::cout << z << std::endl;
-    std::cout << sizeof(x) << std::endl;
-    std::cout << sizeof(y) << std::endl;
-    std::cout << sizeof(z) << std::endl;
+#include <cstdlib>
+#include <iostream>
+#include <typeinfo>
+
+template<class T>
+std::string get_type_char_string() {
+    return std::string(jakilid::traits::type_char<T>::value, 2);
 }
 
+TEST(typing, test_type_chars) {
+    ASSERT_EQ("u0", get_type_char_string<unsigned char>());
+    ASSERT_EQ("s0", get_type_char_string<decltype("abcef")>());
+    ASSERT_EQ("w0", get_type_char_string<decltype(L"abcef")>());
+    ASSERT_EQ("b0", get_type_char_string<decltype(false)>());
+    ASSERT_EQ("f1", get_type_char_string<double>());
+}
+
+using value_types = std::tuple<
+    float,
+    char,
+    double,
+    long,
+    unsigned long,
+    unsigned short,
+    short,
+    long long,
+    long double,
+    std::wstring,
+    unsigned int,
+    wchar_t,
+    std::string,
+    unsigned long long,
+    int
+>;
+
+using pair1 = jakilid::test::construct_pair_tuple<char, value_types>::type;
+using pair2 = jakilid::test::construct_pair_tuple<std::string, value_types>::type;
+using pair3 = jakilid::test::construct_pair_tuple<int, value_types>::type;
+
+using all_pairs = jakilid::test::concat_tuple<pair1, pair2, pair3>::type;
+
+template<class T>
+class TypingFindInsert : public testing::Test {
+public:
+    using key_type = typename T::first_type;
+    using value_type = typename T::second_type;
+    using another_type = typename jakilid::test::tuple_another<value_type, value_types>::type;
+};
+
+
+using test_types = jakilid::test::tuple_to_gtest_types<all_pairs, ::testing::Types>::types;
+
+TYPED_TEST_SUITE(TypingFindInsert, test_types);
+
+TYPED_TEST(TypingFindInsert, print) {
+    std::cout << typeid(typename TestFixture::key_type).name() << std::endl;
+    std::cout << typeid(typename TestFixture::value_type).name() << std::endl;
+    std::cout << typeid(typename TestFixture::another_type).name() << std::endl;
+}
+
+/*
 TEST(sample, sample2) {
     jakilid::SharedDict d("shd");
 
     d.Insert(-10, L"امیرعباس");
-    d.Insert(false, 1.0/4);
+    d.Insert<float>(false, 1.0/4);
 
     std::wstring res;
 
@@ -45,3 +100,4 @@ TEST(sample, sample2) {
     std::cout << (int)gh << std::endl;
 
 }
+*/
---
+++ test/unit/utils/mp.hpp
@@ -0,0 +1,77 @@
+#ifndef JAKILID_TEST_MP_HPP
+#define JAKILID_TEST_MP_HPP
+
+#include <type_traits>
+#include <utility>
+#include <tuple>
+
+namespace jakilid {
+
+namespace test {
+
+template<class T, class U ,class ...Ts> 
+struct another {
+    using type = std::conditional_t<std::is_same_v<T, U>,
+            typename another<T, Ts...>::type, U>;
+};
+
+template<class T, class U>
+struct another<T, U> {
+    using type = std::conditional_t<std::is_same_v<T, U>, void, U>;
+};
+
+template<class T, class U>
+struct tuple_another;
+
+template<class T, class ...Ts>
+struct tuple_another<T, std::tuple<Ts...>> : another<T, Ts...> {};
+
+static_assert(std::is_same_v<another<bool, int, int, bool>::type, int>);
+static_assert(std::is_same_v<another<float, float, float, float>::type, void>);
+static_assert(std::is_same_v<tuple_another<int, std::tuple<int, short, double>>::type, short>);
+
+template<class First, class T>
+struct construct_pair_tuple;
+
+template<class First, class ...Ts>
+struct construct_pair_tuple<First, std::tuple<Ts...>> {
+    using type = std::tuple<std::pair<First, Ts>...>;
+};
+
+template <class ...Args>
+struct concat_tuple;
+
+template <class... First, class... Second, class ...Args>
+struct concat_tuple<std::tuple<First...>, std::tuple<Second...>, Args...> {
+    using type = typename concat_tuple<std::tuple<First..., Second...>, Args...>::type;
+};
+
+template <class... First, class... Second>
+struct concat_tuple<std::tuple<First...>, std::tuple<Second...>> {
+    using type = std::tuple<First..., Second...>;
+};
+
+
+static_assert(std::is_same_v<
+    std::tuple<int, float, char, double, void, void>
+    , concat_tuple<
+        std::tuple<int, float>,
+        std::tuple<char, double, void>,
+        std::tuple<void>
+    >::type
+>);
+
+template<class Tuple, template<class...> class GTestType>
+struct tuple_to_gtest_types;
+
+template <template<class...> class GTestType, class ...Ts>
+struct tuple_to_gtest_types<std::tuple<Ts...>, GTestType>
+{
+    using types = GTestType<Ts...>;
+};
+
+}
+
+}
+
+#endif
